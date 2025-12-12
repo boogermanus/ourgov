1138e49a4bea63c4ac0a818d6098fe33801d3774
Jordan Woodruff <boogermanus@gmail.com>
2025-12-12 15:30:37 -0600

    base layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatInputModule, MatInputModule, MatButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
