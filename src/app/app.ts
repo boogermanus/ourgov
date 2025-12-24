6b4c6578d8049d0f3dd1924c70a2c8849c7980ae
Jordan Woodruff <jwoodruff@datascan.com>
2025-12-24 13:42:47 -0600

    form submits

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,29 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
+import {HttpClient} from '@angular/common/http';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [MatInputModule, MatInputModule, MatButton],
+  imports: [MatInputModule, MatInputModule, MatButton, ReactiveFormsModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  standalone: true
 })
 export class App {
-  protected readonly title = signal('OurGov');
+  protected readonly form: FormGroup
+  protected readonly addressControl: FormControl<string>;
+  protected readonly title = signal('Welcome to OurGov');
+  protected readonly httpClient: HttpClient = inject(HttpClient);
+
+  constructor() {
+    this.addressControl = new FormControl<string>('', {nonNullable: true});
+    this.form = new FormGroup({
+      address: this.addressControl
+    });
+  }
+  protected onSubmit() {
+    console.log(this.form.value);
+  }
 }
