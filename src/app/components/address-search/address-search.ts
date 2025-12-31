b1d93f5c87c1cf356494e696819d31a734fead45
Jordan Woodruff <boogermanus@gmail.com>
2025-12-31 17:45:39 -0600

    show json

--- src/app/components/address-search/address-search.ts
+++ src/app/components/address-search/address-search.ts
@@ -3,10 +3,12 @@ import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} fr
 import {MatInputModule} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
 import {Geocoding} from '../../services/geocoding.service';
+import {Observable} from 'rxjs';
+import {AsyncPipe, JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-address-search',
-  imports: [MatInputModule, MatInputModule, MatButton, ReactiveFormsModule, FormsModule],
+  imports: [MatInputModule, MatInputModule, MatButton, ReactiveFormsModule, FormsModule, JsonPipe, AsyncPipe],
   templateUrl: './address-search.html',
   styleUrl: './address-search.scss',
 })
@@ -14,6 +16,7 @@ export class AddressSearch {
   protected readonly form: FormGroup
   protected readonly addressControl: FormControl<string>;
   protected readonly geoCodingService: Geocoding = inject(Geocoding);
+  protected geocodingData!: Observable<any>;
   constructor() {
     this.addressControl = new FormControl<string>('', {nonNullable: true, validators: [Validators.required]});
     this.form = new FormGroup({
@@ -22,12 +25,6 @@ export class AddressSearch {
   }
 
   protected onSubmit() {
-    this.geoCodingService.getGeocodingData(this.addressControl.value).subscribe(
-      {
-        next: response => {
-          console.log(response);
-        }
-      }
-    )
+    this.geocodingData = this.geoCodingService.getGeocodingData(this.addressControl.value);
   }
 }
